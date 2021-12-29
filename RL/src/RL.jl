3a714c801a91c25bcd8bb48d4e736204e6670b89
Neel Smith <dnsmith.neel@gmail.com>
2021-12-29 05:00:13 -0500

    Add comments

--- RL/src/RL.jl
+++ RL/src/RL.jl
@@ -1,7 +1,13 @@
+# ReadingList demo from #Code from https://cite-architecture.github.io/CitableBase.jl/stable
 module RL
 
+#Code from https://cite-architecture.github.io/CitableBase.jl/stable/urns/
 include("page1_urn.jl")
+
+#Code from  https://cite-architecture.github.io/CitableBase.jl/stable/citable/
 include("page2_citable.jl")
+
+#Code from https://cite-architecture.github.io/CitableBase.jl/stable/collections/
 include("page3_collection.jl")
 
 export Isbn10Urn
